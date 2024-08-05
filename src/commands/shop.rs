5e4ee2d1929c6e2b8946869a0c9737fd1a19e720
skepz <skepz.dev@gmail.com>
2024-08-05 17:18:59 -0500

    add comment

--- src/commands/shop.rs
+++ src/commands/shop.rs
@@ -15,7 +15,7 @@ pub async fn run(ctx: &Context, cmd: &CommandInteraction) {
         .fields(vec![
             ("Your Super Nanners:", format!("{}:zap:", user_file.get_super_nanners()), false),
             // todo: make super drill upgradable rather than a one time purchase (10 x tier)
-            ("1: Super Drill", if user_file.has_super_drill() { "You already own this item!" } else { "15:zap:" }.to_string(), true),
+            ("1: Super Drill", if user_file.has_super_drill() { "You already own this item!" } else { "15:zap:" }.to_string(), true), // todo: super drill upgrades
             ("2: Minion", "10:zap:".to_string(), true),
             ("3: Spell Tome (Fireball 25-40hp)", "5:zap:".to_string(), true),
             ("4: Spell Tome (Mighty Winds 5-25hp)", "3:zap:".to_string(), true),
